b90921e307a31c94889d465dfb88471418c2f9ae
magfur2309 <magfurgezo@gmail.com>
2025-02-16 00:34:18 +0700

    app.py

--- app.py
+++ app.py
@@ -14,22 +14,44 @@ def extract_data_from_pdf(pdf_file):
             if text:
                 lines = text.split('\n')
                 
-                # Parsing data dari teks
-                if "Faktur Pajak" in text:
-                    no_fp = lines[3].split(':')[-1].strip()
-                    nama_penjual = lines[5].split(':')[-1].strip()
-                    nama_pembeli = lines[10].split(':')[-1].strip()
-                    barang = lines[17].strip()
-                    harga = int(lines[18].split('x')[0].replace('Rp ', '').replace(',', ''))
-                    unit = "Bulan"
-                    qty = int(lines[18].split('x')[-1].split('Bulan')[0].strip())
-                    total = harga * qty
-                    dpp = int(lines[22].split()[-1].replace(',', ''))
-                    ppn = int(lines[24].split()[-1].replace(',', ''))
-                    tanggal_faktur = lines[-4].split(',')[-1].strip()
+                try:
+                    # Parsing data dari teks
+                    no_fp = lines[3].split(':')[-1].strip() if len(lines) > 3 else ""
+                    nama_penjual = lines[5].split(':')[-1].strip() if len(lines) > 5 else ""
+                    nama_pembeli = lines[10].split(':')[-1].strip() if len(lines) > 10 else ""
+                    barang = lines[17].strip() if len(lines) > 17 else ""
+
+                    try:
+                        harga = int(lines[18].split('x')[0].replace('Rp ', '').replace(',', '')) if len(lines) > 18 else 0
+                    except ValueError:
+                        harga = 0
                     
+                    unit = "Bulan"
+
+                    try:
+                        qty = int(lines[18].split('x')[-1].split('Bulan')[0].strip()) if len(lines) > 18 else 0
+                    except ValueError:
+                        qty = 0
+                    
+                    total = harga * qty
+
+                    try:
+                        dpp = int(lines[22].split()[-1].replace(',', '')) if len(lines) > 22 else 0
+                    except ValueError:
+                        dpp = 0
+
+                    try:
+                        ppn = int(lines[24].split()[-1].replace(',', '')) if len(lines) > 24 else 0
+                    except ValueError:
+                        ppn = 0
+
+                    tanggal_faktur = lines[-4].split(',')[-1].strip() if len(lines) > 4 else ""
+
                     data.append([no_fp, nama_penjual, nama_pembeli, barang, harga, unit, qty, total, dpp, ppn, tanggal_faktur])
-    
+                
+                except Exception as e:
+                    print(f"Terjadi kesalahan dalam membaca halaman: {e}")
+
     return data
 
 # Streamlit UI
